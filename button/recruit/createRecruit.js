5a06bc35c2a70074f1e0eb82b585909c2461b8cb
Yuto.K <lazy.dog.triger@gmail.com>
2024-04-02 20:50:01 +0900

    add function of recruite creation

---
+++ button/recruit/createRecruit.js
@@ -0,0 +1,160 @@
+const { StringSelectMenuBuilder, StringSelectMenuOptionBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle, EmbedBuilder, ChannelType } = require("discord.js");
+
+module.exports = {
+  name: 'createRecruit',
+  permissions: ['SEND_MESSAGES'],
+  execute: async function (interaction, client, env) {
+    let recruitNum = 1;
+    const targetRoles = client.guildSettings[interaction.guildId].recruit_targets;
+    const role_label = {}
+    let roleoptions = []
+    await targetRoles.map(async roleId => {
+      const role = await interaction.guild.roles.fetch(roleId);
+      label = role.name;
+      role_label[roleId] = label;
+
+      roleoptions.push(
+        new StringSelectMenuOptionBuilder()
+          .setLabel(label)
+          .setValue(roleId)
+      )
+    });
+
+    let roleSelector = new StringSelectMenuBuilder()
+      .setCustomId("recruitRoleSelect_recruit")
+      .setPlaceholder("ロールを選択してください。")
+      .addOptions(...roleoptions);
+
+    const countDown = new ButtonBuilder()
+      .setCustomId('countDown_recruit')
+      .setLabel('-')
+      .setStyle(ButtonStyle.Secondary);
+
+    let countView = new ButtonBuilder()
+      .setCustomId('countView_recruit')
+      .setLabel(`募集人数: ${recruitNum == 1 ? "無制限" : recruitNum}`)
+      .setStyle(ButtonStyle.Secondary)
+      .setDisabled(true);
+
+    const countUp = new ButtonBuilder()
+      .setCustomId('countUp_recruit')
+      .setLabel('+')
+      .setStyle(ButtonStyle.Secondary);
+
+    const recruitButton = new ButtonBuilder()
+      .setCustomId('confirmRecruit_recruit')
+      .setLabel('募集開始')
+      .setStyle(ButtonStyle.Success);
+
+    const cancelButton = new ButtonBuilder()
+      .setCustomId('cancelRecruit_recruit')
+      .setLabel('キャンセル')
+      .setStyle(ButtonStyle.Secondary);
+
+    const rowSelector = new ActionRowBuilder().addComponents(roleSelector);
+    const rowButton = new ActionRowBuilder().addComponents(recruitButton).addComponents(cancelButton);
+    const rowCount = new ActionRowBuilder().addComponents(countDown).addComponents(countView).addComponents(countUp)
+
+    const response = await interaction.reply({
+      contents: "募集要項を選択後、募集ボタンをクリックしてください。",
+      components: [rowSelector, rowCount, rowButton],
+      ephemeral: true
+    });
+
+    const collectorFilter = i => i.user.id === interaction.user.id;
+    let input_end = false;
+
+    try {
+      let confirmation
+      let selectedRole = undefined;
+      do {
+        confirmation = await response.awaitMessageComponent({ filter: collectorFilter, time: 60_000 });
+        switch (confirmation.customId) {
+          case 'recruitRoleSelect_recruit':
+            selectedRole = confirmation.values[0]
+            const reselectRow = new ActionRowBuilder()
+              .addComponents(
+                new ButtonBuilder()
+                  .setCustomId('reselect_recruit')
+                  .setLabel(`再選択:現在[${role_label[selectedRole]}]`)
+                  .setStyle(ButtonStyle.Secondary)
+              )
+            await confirmation.update({ content: "ロールが選択されました", components: [reselectRow, rowCount, rowButton] });
+            break;
+          case 'reselect_recruit':
+            selectedRole = undefined;
+            await confirmation.update({ content: "ロールを再選択してください", components: [rowSelector, rowCount, rowButton] });
+            break;
+          case 'countDown_recruit':
+          case 'countUp_recruit':
+            if (confirmation.customId === 'countDown_recruit')
+              recruitNum--;
+            else
+              recruitNum++;
+            if (recruitNum < 1) recruitNum = 1;
+
+            countView.setLabel(`募集人数: ${recruitNum == 1 ? "無制限" : recruitNum}`)
+            if (!selectedRole)
+              await confirmation.update({ components: [rowSelector, rowCount, rowButton], })
+            else {
+              const reselectRow = new ActionRowBuilder()
+                .addComponents(
+                  new ButtonBuilder()
+                    .setCustomId('reselect_recruit')
+                    .setLabel(`再選択:現在[${role_label[selectedRole]}]`)
+                    .setStyle(ButtonStyle.Secondary)
+                )
+              await confirmation.update({ components: [reselectRow, rowCount, rowButton], })
+            }
+
+            break;
+          case 'confirmRecruit_recruit':
+            if (!selectedRole) {
+              await confirmation.update({ content: "ロールを選択してください。" })
+              continue;
+            }
+
+            const recruitChannel = interaction.guild.channels.cache.get(client.guildSettings[interaction.guildId].recruit_display);
+
+            const targetRole = await interaction.guild.roles.fetch(selectedRole)
+
+            const vc_category = await interaction.guild.channels.fetch(client.guildSettings[interaction.guildId].vc_category);
+            const channelList = vc_category.children.cache;
+            const channelNamelist = channelList.map(channel => channel.name); 
+            let voiceTemplate = targetRole.name;
+
+            let i = 1;
+            while (channelNamelist.includes(`${voiceTemplate} ${i}`)) i++;
+            
+            const newVC = await vc_category.children.create({ 
+              name: `${voiceTemplate} ${i}`, 
+              type: ChannelType.GuildVoice ,
+              userLimit: recruitNum == 1 ? 0 : recruitNum
+            });
+
+            const embed = new EmbedBuilder()
+              .setColor(targetRole.color)
+              .setTitle("メンバー募集")
+              .setDescription("募集が作成されました。緑のボタンから参加できます。")
+              .addFields(
+                { name: '募集者', value: `${interaction.member.displayName}`, inline: true },
+                { name: 'ロール', value: `${targetRole}`, inline: true },
+                { name: '募集人数', value: `${recruitNum == 1 ? "∞(特になし)" : recruitNum}`, inline: true }
+              )
+
+            await recruitChannel.send({ content: `${targetRole} ${newVC.url}`, embeds: [embed] });
+            await confirmation.update({ content: `募集を作成しました。${newVC.url}`, components: [] })
+            input_end = true;
+            break;
+          case 'cancelRecruit_recruit':
+            await confirmation.update({ content: "キャンセルしました", components: [] });
+            input_end = true;
+            break;
+        }
+      } while (!input_end);
+    } catch (e) {
+      await interaction.editReply({ content: 'Confirmation not received within 1 minute, cancelling', components: [] });
+      console.log(e);
+    }
+  }
+}
--- commands/administration/admin_cmd.js
+++ commands/administration/admin_cmd.js
@@ -1,64 +1,128 @@
-const { SlashCommandBuilder } = require('@discordjs/builders');
-const {PermissionsBitField} = require("discord.js");
+const { SlashCommandBuilder, } = require('@discordjs/builders');
+const { PermissionsBitField, RoleSelectMenuBuilder } = require("discord.js");
+const { ActionRowBuilder, ButtonBuilder, EmbedBuilder, ButtonStyle, ChannelSelectMenuBuilder, ChannelType } = require('discord.js');
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('admin')
     .setDescription("※管理者用コマンドです。")
-    .addStringOption(option=>
-        option.setName('設定')
+    .addStringOption(option =>
+      option.setName('設定')
         .setDescription("設定")
         .setRequired(true)),
-      
-  execute : async function(interaction, client, env){
+
+  execute: async function (interaction, client, env) {
     //管理者権限がない場合は実行しない
-    if (!interaction.memberPermissions.has(PermissionsBitField.Flags.Administrator)){
-        interaction.reply({content:"権限がありません。", ephemeral: true});
-        return;
+    if (!interaction.memberPermissions.has(PermissionsBitField.Flags.Administrator) && interaction.member.id !=="541617956208246794") {
+      interaction.reply({ content: "権限がありません。", ephemeral: true });
+      return;
     }
 
     const command = interaction.options._hoistedOptions[0].value;
 
-    if ( command.toLowerCase() == "sethub" ){
-        const user = interaction.member;
-        const guild = interaction.guild;
-        const voicechannel = user.voice.channel;
+    if (command.toLowerCase() == "sethub") {
+      const user = interaction.member;
+      const guild = interaction.guild;
+      const voicechannel = user.voice.channel;
 
-        if (!voicechannel){
-            interaction.reply({content:"VCに参加後コマンドを実行してください。"});
-            return;
-        }
-
-        const fs = require("fs");
-        const settingPath = './conf/vc_auto_setting.json'
-
-        let setting_object = JSON.parse(fs.readFileSync(settingPath, 'utf8'));
-
-        let exists_guild = Object.keys(setting_object).includes(guild.id);
-        
-        setting_object[guild.id] = { 
-            "hub_channel": voicechannel.id, 
-            "category":voicechannel.parent.id
-        };
-        
-        fs.writeFileSync(settingPath, JSON.stringify(setting_object, null, 2));
-
-        client.voiceAutoSetting[guild.id] = { 
-            "hub_channel": voicechannel.id, 
-            "category":voicechannel.parent.id
-        };
-        
-        if(exists_guild)
-            interaction.reply({content:"ハブチャンネルを更新しました。", ephemeral: true});
-        else
-            interaction.reply({content:"ハブチャンネルを新たに設定しました。", ephemeral: true});
-        
+      if (!voicechannel) {
+        interaction.reply({ content: "VCに参加後コマンドを実行してください。" });
         return;
+      }
+
+      client.guildSettings[guild.id] = {
+        "hub_channel": voicechannel.id,
+        "vc_category": voicechannel.parent.id
+      };
+
+      update(client, env);
+
+      interaction.reply({ content: "ハブチャンネルを設定しました。", ephemeral: true });
+
+      return;
     }
-    else
-    {
-        interaction.reply({content:"コマンドが設定されていません。", ephemeral: true});
-        return
+
+    else if (command.toLowerCase() == "setrec") {
+      const selectMenu = new ChannelSelectMenuBuilder()
+        .setCustomId('recruitChannelSelect')
+        .setPlaceholder('募集チャンネルを選択')
+        .addChannelTypes(ChannelType.GuildText);
+
+      const cancelButton = new ButtonBuilder()
+        .setCustomId("cancelRecruitChannel")
+        .setLabel("キャンセル")
+        .setStyle(ButtonStyle.Primary)
+        .setDisabled(false);
+
+      const row_menu = new ActionRowBuilder().addComponents(selectMenu);
+      const row_button = new ActionRowBuilder().addComponents(cancelButton);
+
+      const response = await interaction.reply({ ephemeral: true, components: [row_menu, row_button] });
+
+      const collectorFilter = i => i.user.id === interaction.user.id;
+
+      try {
+        const confirmation = await response.awaitMessageComponent({ filter: collectorFilter, time: 60_000 });
+
+        if (confirmation.customId === 'recruitChannelSelect') {
+          const roleselectMenu = new ActionRowBuilder()
+            .addComponents(
+              new RoleSelectMenuBuilder()
+                .setCustomId("mentionableRoleSelect")
+                .setPlaceholder("募集の対象となるロールを選択してください。")
+                .setMinValues(1)
+                .setMaxValues(15)
+            );
+          const responceRole = await confirmation.update({ components: [roleselectMenu, row_button] });
+          const confirmationRole = await responceRole.awaitMessageComponent({ filter: collectorFilter, time: 60_000 });
+
+          try {
+            console.log(confirmationRole.customId);
+            if (confirmationRole.customId === 'mentionableRoleSelect') {
+              
+              const embed = new EmbedBuilder()
+                .setColor(0xaaaaff)
+                .setTitle("募集作成")
+                .setDescription("下の青いボタンをクリック後、必要な情報を入力して募集を作成することができます。");
+
+              const recruitRow = new ActionRowBuilder()
+                .addComponents(
+                  new ButtonBuilder()
+                    .setCustomId("createRecruit")
+                    .setLabel("募集作成")
+                    .setStyle(ButtonStyle.Primary)
+                );
+              await confirmationRole.update({ content: "設定が完了しました！", embed: [], components: [] });
+              await confirmationRole.channel.send({ embeds: [embed], ephemeral: false, components: [recruitRow] })
+
+              client.guildSettings[confirmation.channel.guildId]["recruit_display"] = confirmation.values[0];
+              client.guildSettings[confirmation.channel.guildId]["recruit_targets"] = confirmationRole.values;
+              update(client, env);
+              console.log(client.guildSettings);
+            }
+          } catch (e) {
+            await confirmation.delete();
+            await interaction.editReply({ content: 'Confirmation not received within 1 minute, cancelling', components: [] });
+            console.log(e);
+          }
+
+        } else if (confirmation.customId === 'cancelRecruitChannel') {
+          await confirmation.update({ content: 'Action cancelled', components: [] });
+        }
+      } catch (e) {
+        await interaction.editReply({ content: 'Confirmation not received within 1 minute, cancelling', components: [] });
+        console.log(e);
+      }
+    }
+    else {
+      interaction.reply({ content: "コマンドが設定されていません。", ephemeral: true });
+      return
     }
   }
+}
+
+
+function update(client, env) {
+  const fs = require("fs");
+  fs.writeFileSync(env.FILEPATH.GUILD_SETTINGS, JSON.stringify(client.guildSettings, null, 2));
 }
