1e24d33c5709e902c74367e9d5fb69297691efa0
Yuto.K <lazy.dog.triger@gmail.com>
2024-04-02 20:44:47 +0900

    remove logging from limset

--- commands/voice/limset.js
+++ commands/voice/limset.js
@@ -20,25 +20,11 @@ module.exports = {
     
     let n = interaction.options._hoistedOptions[0].value;
 
-    if(n!=0)
-    {
+    if(n!=0){
       const m = vc.members.size;
       n = Math.max(m,n);
     }
-    
-
     vc.setUserLimit(n);
-    
-    const actorname = interaction.user.username
-    var date = new Date();
-    date=date.toLocaleString();
-
-    console.log("text file is writing...")
-    fs.appendFileSync(env.limitVoiceLogFile, `[${date}]「${actorname}」 has changed 「${vc.name}」's (in ${vc.parent.name}) limit into ${n!=0?n:'∞'}.\n`);
-    console.log("text file was completely saved!")
-
-    const vcobj = {channel_name:vc.name, parent_name:vc.parent.name, limit:n};
-    const obj = {date:date, name:actorname, settings:vcobj};
 
     interaction.reply({content:`人数制限を${n!=0?n:"∞"}人に変更しました。`, ephemeral: true});
   }
